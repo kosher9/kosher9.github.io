60756cc76e83809ef88d7d64feee0b5d97fc85cb
kosher <lionel.kossi@gmail.com>
2022-03-24 01:19:17 +0000

    Create tasks list dynamically

--- src/index.js
+++ src/index.js
@@ -1,34 +1,67 @@
-import './style.css'
-import Image from './img/refresh.png'
+import "./style.css";
+import Image from "./img/refresh.png";
+import Task from "./modules/task";
 
 // Font Awesome 5 (Free)
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid' // https://fontawesome.com/icons?d=gallery&s=solid&m=free
-import '@fortawesome/fontawesome-free/js/regular' // https://fontawesome.com/icons?d=gallery&s=regular&m=free
-import '@fortawesome/fontawesome-free/js/brands' // https://fontawesome.com/icons?d=gallery&s=brands&m=free
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid"; // https://fontawesome.com/icons?d=gallery&s=solid&m=free
+import "@fortawesome/fontawesome-free/js/regular"; // https://fontawesome.com/icons?d=gallery&s=regular&m=free
+import "@fortawesome/fontawesome-free/js/brands"; // https://fontawesome.com/icons?d=gallery&s=brands&m=free
 
-class Task{
+const container = document.getElementById("ctn-task-list");
 
-    constructor(index, completed, description){
-        this.index = index
-        this.completed = completed
-        this.description = description
-    }
+let task1 = new Task(1, false, "wash the dishes");
+let task2 = new Task(0, false, "complete To Do list project");
+let task3 = new Task(3, false, "Let's do one more");
 
-}
+let taskList = [task1, task2, task3];
 
-let task1 = Task(0, false, "wash the dishes")
-let task2 = Task(1, false, "complete To Do list project")
-let task3 = Task(3, false, "Let's do one more")
+const orderTasks = (listTask) => {
+  return listTask.sort((a, b) => {
+    return a.index - b.index;
+  });
+};
 
-let taskList = [task1, task2, task3]
+const populateHtml = (tasks) => {
 
-const populateHtml = (listTask) => {
-    listTask.array.forEach(element => {
-        
-    });
-}
+  tasks.forEach((element) => {
 
-window.addEventListener('load', () => {
-    populateHtml(taskList)
-})
+    const li = document.createElement("li");
+    const fDiv = document.createElement("div");
+    const sDiv = document.createElement("div");
+    const fIpt = document.createElement("input");
+    const sIpt = document.createElement("input");
+    const iDiv = document.createElement("div");
+    const icon = document.createElement("i");
+    const line = document.createElement("div");
+
+    fDiv.className = "ctn-task";
+    sDiv.className = "ctn-left";
+    fIpt.className = "checkbox";
+    sIpt.className = "description";
+    iDiv.className = "ctn-icon";
+    icon.className = "fa-solid fa-ellipsis-vertical";
+    line.className = "line";
+
+    fIpt.setAttribute("type", "checkbox")
+
+    sIpt.setAttribute("type", "text")
+    sIpt.value = element.description
+
+    sDiv.appendChild(fIpt)
+    sDiv.appendChild(sIpt)
+    iDiv.appendChild(icon)
+    fDiv.appendChild(sDiv)
+    fDiv.appendChild(iDiv)
+    li.appendChild(fDiv)
+    li.appendChild(line)
+    container.appendChild(li)
+
+  });
+
+};
+
+window.addEventListener("load", () => {
+  populateHtml(orderTasks(taskList));
+  console.log(e);
+});
---
+++ src/modules/task.js
@@ -0,0 +1,9 @@
+export default class Task{
+
+    constructor(index, completed, description){
+        this.index = index
+        this.completed = completed
+        this.description = description
+    }
+
+}
